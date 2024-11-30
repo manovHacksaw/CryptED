5a065cdd6bdfc6b1dbcab80e42119818817d8bdc
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2024-11-30 20:31:08 +0530

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ const geistMono = localFont({
 
 // Metadata for the page
 export const metadata: Metadata = {
-  title: 'Daccy',
+  title: 'CryptED',
   description: 'Learn coding efficiently',
 };
 
